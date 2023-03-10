5befc641f5af2d5a783467c1698b01eb35f44ba1
AntonisTaramigkos <124712913+AntonisTaramigkos@users.noreply.github.com>
2023-03-10 10:23:35 +0200

    Update #Australian Open.py

--- #Australian Open.py
+++ #Australian Open.py
@@ -59,13 +59,47 @@ def match_making():
     
 match_making()
 
-# player1 = Players("Roger Federer", "Switzerland", 5.0)
-# player2 = Players("Antonis Taramigkos","Greece", 10)
+### Ayth einai i geniki idea gia to epomeno erotima me ta matchmakings apo tin 17-128 8esi...
+### Alla o kodikas 8a bgei terastios.... ###
+###Prospa8o na do pos 8a to kano se Function/Class se sindiasmo me ena for/while loop ###
+### Unfinished!! ###
 
-# print the player's information
-# print(player1)
-# print(player2)
-# match_making()
+
+#MAtch_Making_from_7_teams
+import random
+team1 = ['name11', 'name12', 'name13', 'name14', 'name15', 'name16', 'name17', 'name18', 'name19', 'name110', 'name111', 'name112', 'name113', 'name114', 'name115', 'name116']
+team2 = ['name21', 'name22', 'name23', 'name24', 'name25', 'name26', 'name27', 'name28', 'name29', 'name210', 'name211', 'name212', 'name213', 'name214', 'name215', 'name216']
+team3 = ['name31', 'name32', 'name33', 'name34', 'name35', 'name36', 'name37', 'name38', 'name39', 'name310', 'name311', 'name312', 'name313', 'name314', 'name315', 'name316']
+team4 = ['name41', 'name42', 'name43', 'name44', 'name45', 'name46', 'name47', 'name48', 'name49', 'name410', 'name411', 'name412', 'name413', 'name414', 'name415', 'name416']
+team5 = ['name51', 'name52', 'name53', 'name54', 'name55', 'name56', 'name57', 'name58', 'name59', 'name510', 'name511', 'name512', 'name513', 'name514', 'name515', 'name516']
+team6 = ['name61', 'name62', 'name63', 'name64', 'name65', 'name66', 'name67', 'name68', 'name69', 'name610', 'name611', 'name612', 'name613', 'name614', 'name615', 'name616']
+team7 = ['name71', 'name72', 'name73', 'name74', 'name75', 'name76', 'name77', 'name78', 'name79', 'name710', 'name711', 'name712', 'name713', 'name714', 'name715', 'name716']
+
+match_making1 =[]
+match_making2 =[]
+match_making3 =[]
+match_making4 =[]
+match_making5 =[]
+match_making6 =[]
+match_making7 =[]
+
+all_teams =[team1, team2, team3, team4, team5,team6,team7]
+for team in range(len(all_teams)):
+    
+name1 = random.sample(team1,1)
+name2 = random.sample(team2,1)
+match_making1.append(name1)
+match_making1.append(name2)
+
+team1.remove(name1[0])
+team2.remove(name2[0])
+
+print(match_making1)
+print(team1)
+print(team2)
+
+
+
 
 
 
