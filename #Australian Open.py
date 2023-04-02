39716488d04b927b9a4eb9c0d9deef8d82f980e2
AntonisTaramigkos <124712913+AntonisTaramigkos@users.noreply.github.com>
2023-04-02 19:40:54 +0300

    Update #Australian Open.py
    Καλησπέρα διόρθωσα τον κώδικα !

--- #Australian Open.py
+++ #Australian Open.py
@@ -1,7 +1,9 @@
-#Australian Open
-import random
+###Na doyme an doulebei to match making tou protou girou gia ola ta group
+### ka8e fora poy trexei moy allazei ta apotelesmata
+## mallon to index prepi na to trexo me ena for loop?
 
-players_list1 = []
+import random
+  
 
 aopen_top_16 = {"Rafael Nadal":("Spain",1),
 "Casper Ruud":("Norway",2),
@@ -20,54 +22,64 @@ aopen_top_16 = {"Rafael Nadal":("Spain",1),
 "Jannik Sinner":("Italy",15),
 "Frances Tiafoe":("United States of America",16)}
 
-#playerClass
-class Players():
-    def __init__(self,name,country,world_ranking) :
-        self.name = name
-        self.country = country
-        self.world_ranking = world_ranking
-        pass
+top_players_list1 = []
+top_players_list = list(aopen_top_16.keys())
+# print("TOP PLAYERLIST:",top_players_list)
 
-    def __repr__(self) -> str:
-        return "{:<25}  {:<15}  {:>10.1f}".format(self.name, self.country, self.world_ranking)
-        pass
 
-#Match Making#
-#From a List/Dict or SQL db we add the players in the a list
-#the with a radom choose we add them in a new list Match_Making
-#then we take the duos and with a print we shaw the pairs
-def match_making():
-    players_list = list(aopen_top_16.keys())
-    
-    print(players_list)
+team0 = top_players_list
+team1 = ['name11', 'name12', 'name13', 'name14', 'name15', 'name16', 'name17', 'name18', 'name19', 'name110', 'name111', 'name112', 'name113', 'name114', 'name115', 'name116']
+team2 = ['name21', 'name22', 'name23', 'name24', 'name25', 'name26', 'name27', 'name28', 'name29', 'name210', 'name211', 'name212', 'name213', 'name214', 'name215', 'name216']
+team3 = ['name31', 'name32', 'name33', 'name34', 'name35', 'name36', 'name37', 'name38', 'name39', 'name310', 'name311', 'name312', 'name313', 'name314', 'name315', 'name316']
+team4 = ['name41', 'name42', 'name43', 'name44', 'name45', 'name46', 'name47', 'name48', 'name49', 'name410', 'name411', 'name412', 'name413', 'name414', 'name415', 'name416']
+team5 = ['name51', 'name52', 'name53', 'name54', 'name55', 'name56', 'name57', 'name58', 'name59', 'name510', 'name511', 'name512', 'name513', 'name514', 'name515', 'name516']
+team6 = ['name61', 'name62', 'name63', 'name64', 'name65', 'name66', 'name67', 'name68', 'name69', 'name610', 'name611', 'name612', 'name613', 'name614', 'name615', 'name616']
+team7 = ['name71', 'name72', 'name73', 'name74', 'name75', 'name76', 'name77', 'name78', 'name79', 'name710', 'name711', 'name712', 'name713', 'name714', 'name715', 'name716']
+
+
+
+def omiloi(all_teams):
+    match_making = []
+    for i in range(8):
+        match = []
+        for team in all_teams:
+            name = random.sample(team, 1)[0]
+            match.append(name)
+            team.remove(name)
+        match_making.append(match)
+    return match_making
+
+
+
+all_teams = [team0, team1, team2, team3, team4, team5, team6, team7]
+
+match_making = omiloi(all_teams)
+
+def make_matches(match_making, match_index):
+    omilos_couples = []
+    omilos_couples.extend(match_making[match_index])
+    print(omilos_couples)
     couples = []
-
-    while len(players_list) > 1:
+    
+    while len(omilos_couples) > 1:
         # randomly select two names from the list
-        names = random.sample(players_list, 2)
+        names = random.sample(omilos_couples, 2)
 
         # remove the selected names from the list
-        players_list.remove(names[0])
-        players_list.remove(names[1])
+        omilos_couples.remove(names[0])
+        omilos_couples.remove(names[1])
 
         # add the pair of names to the couples list
         couples.append((names[0], names[1]))
 
         
-        for couples in couples:
-            print(couples)    
-    
-match_making()
+    for couple in couples:
+         print(couple)    
+    # print(couples)
 
-# player1 = Players("Roger Federer", "Switzerland", 5.0)
-# player2 = Players("Antonis Taramigkos","Greece", 10)
+#make_matches(match_making, 2)
 
-# print the player's information
-# print(player1)
-# print(player2)
-# match_making()
+for num in range(8):
+    print(make_matches(match_making ,num))
 
-
-
-
-
+print(match_making)
